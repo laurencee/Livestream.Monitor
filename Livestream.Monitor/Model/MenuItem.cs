af3d5eb35111fbaba8d2e6a30842897f64f7506f
laurence.evans <laurence.evanz@gmail.com>
2015-10-21 20:17:12 +1000

    Added check mark to indicate the currently selected theme.

--- Livestream.Monitor/Model/MenuItem.cs
+++ Livestream.Monitor/Model/MenuItem.cs
@@ -1,17 +1,40 @@
-using System;
+using Caliburn.Micro;
+using Action = System.Action;
 
 namespace Livestream.Monitor.Model
 {
-    public class MenuItem
+    public class MenuItem : PropertyChangedBase
     {
         private readonly Action action;
+        private string name;
+        private bool isChecked;
 
         public MenuItem(Action action)
         {
             this.action = action;
         }
 
-        public string Name { get; set; }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == name) return;
+                name = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public bool IsChecked
+        {
+            get { return isChecked; }
+            set
+            {
+                if (value == isChecked) return;
+                isChecked = value;
+                NotifyOfPropertyChange();
+            }
+        }
 
         public void Command()
         {
--- Livestream.Monitor/ViewModels/ThemeSelectorViewModel.cs
+++ Livestream.Monitor/ViewModels/ThemeSelectorViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using Caliburn.Micro;
 using Livestream.Monitor.Core;
 using Livestream.Monitor.Model;
@@ -8,6 +10,8 @@ namespace Livestream.Monitor.ViewModels
 {
     public class ThemeSelectorViewModel : Screen
     {
+        private readonly ISettingsHandler settingsHandler;
+
         #region Design time constructor
         public ThemeSelectorViewModel()
         {
@@ -20,6 +24,7 @@ namespace Livestream.Monitor.ViewModels
 
         public ThemeSelectorViewModel(ISettingsHandler settingsHandler)
         {
+            this.settingsHandler = settingsHandler;
             if (settingsHandler == null) throw new ArgumentNullException(nameof(settingsHandler));
 
             foreach (MetroThemeBaseColour themeBaseColour in Enum.GetValues(typeof(MetroThemeBaseColour)))
@@ -28,6 +33,10 @@ namespace Livestream.Monitor.ViewModels
                 {
                     Name = themeBaseColour.ToString().ToFriendlyString()
                 };
+                menuItem.IsChecked = string.Equals(
+                    settingsHandler.Settings.MetroThemeBaseColour.ToString(),
+                    menuItem.Name.Replace(" ", string.Empty)
+                    );
                 BaseThemes.Add(menuItem);
             }
 
@@ -35,8 +44,12 @@ namespace Livestream.Monitor.ViewModels
             {
                 var menuItem = new MenuItem(() => settingsHandler.Settings.MetroThemeAccentColour = themeAccentColour)
                 {
-                    Name = themeAccentColour.ToString().ToFriendlyString()
+                    Name = themeAccentColour.ToString().ToFriendlyString(),
                 };
+                menuItem.IsChecked = string.Equals(
+                    settingsHandler.Settings.MetroThemeAccentColour.ToString(),
+                    menuItem.Name
+                    );
                 AccentColours.Add(menuItem);
             }
         }
@@ -44,5 +57,29 @@ namespace Livestream.Monitor.ViewModels
         public List<MenuItem> BaseThemes { get; } = new List<MenuItem>();
 
         public List<MenuItem> AccentColours { get; } = new List<MenuItem>();
+
+        protected override void OnActivate()
+        {
+            settingsHandler.Settings.PropertyChanged += SettingsOnPropertyChanged;
+            base.OnActivate();
+        }
+
+        private void SettingsOnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(settingsHandler.Settings.MetroThemeAccentColour))
+            {
+                AccentColours.ForEach(x => x.IsChecked = false);
+                var menuItem = AccentColours.FirstOrDefault(x => string.Equals(x.Name, settingsHandler.Settings.MetroThemeAccentColour.ToString()));
+                if (menuItem != null)
+                    menuItem.IsChecked = true;
+            }
+            else if (e.PropertyName == nameof(settingsHandler.Settings.MetroThemeBaseColour))
+            {
+                BaseThemes.ForEach(x => x.IsChecked = false);
+                var menuItem = BaseThemes.FirstOrDefault(x => string.Equals(x.Name, settingsHandler.Settings.MetroThemeBaseColour.ToString()));
+                if (menuItem != null)
+                    menuItem.IsChecked = true;
+            }
+        }
     }
 }
