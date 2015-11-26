846ae67813b51474065af78750a5c0303900a345
laurence.evans <laurence.evanz@gmail.com>
2015-11-26 19:55:11 +1000

    Forgot to move an exception used in testing... :(

--- Livestream.Monitor/ViewModels/ShellViewModel.cs
+++ Livestream.Monitor/ViewModels/ShellViewModel.cs
@@ -65,7 +65,6 @@ namespace Livestream.Monitor.ViewModels
 
         private void WindowMinimized()
         {
-            throw new InvalidOperationException("Test exception");
             Application.Current.MainWindow.Hide();
             if (firstMinimize) // only show the notification one time
             {
