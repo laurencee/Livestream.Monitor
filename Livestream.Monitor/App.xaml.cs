b5973388a499053c25fe4641c96fd21b2665e143
laurence.evans <laurence.evanz@gmail.com>
2015-12-01 21:40:16 +1000

    Added single instance class to only allow 1 instance of the application open at one time.

--- Livestream.Monitor/App.xaml.cs
+++ Livestream.Monitor/App.xaml.cs
@@ -1,21 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using Livestream.Monitor.Core;
 
 namespace Livestream.Monitor
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
-        public App()
+        private const string Unique = "Livestream Monitor";
+
+        [STAThread]
+        public static void Main()
         {
-            InitializeComponent();
+            // Check that only one instance of the application is running
+            if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+            {
+                var application = new App();
+                application.InitializeComponent();
+                application.Run();
+
+                // Allow single instance code to perform cleanup operations
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        /// <summary> Command that will be called if a second attempt to launch the application occurs. </summary>
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            foreach (Window window in Windows)
+            {
+                // Bring window to foreground
+                if (window.WindowState == WindowState.Minimized)
+                {
+                    Current.MainWindow.Show();
+                    window.WindowState = WindowState.Normal;
+                }
+
+                window.Activate();
+            }
+
+            return true;
         }
     }
 }
