0074ccdf992cca79a7146205b18795121d1f9041
laurencee <laurence.evanz@gmail.com>
2016-01-05 14:10:40 +1000

    Updated nuget packages to their latest versions.
    Added a unit test for settings to ensure all declared public properties have the JsonProperty attribute.

---
+++ Livestream.Monitor.Tests/SettingsShould.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Livestream.Monitor.Core;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Livestream.Monitor.Tests
+{
+    public class SettingsShould
+    {
+        public static IEnumerable<object[]> SettingsProperties
+        {
+            get
+            {
+                return typeof(Settings).GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                                        .Select(x => new[] { x });
+            }
+        }
+
+        [Theory]
+        [MemberData("SettingsProperties")]
+        public void AllPublicPropertiesHaveJsonPropertyAttribute(PropertyInfo property)
+        {
+            var jsonProperty = property.GetCustomAttribute<JsonPropertyAttribute>();
+            Assert.NotNull(jsonProperty);
+        }
+    }
+}
