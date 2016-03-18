d30d2a715fc557d1adb680354054f3ffc49b9ae8
laurencee <laurence.evanz@gmail.com>
2016-03-18 22:52:24 +1000

    Bump assembly version patch number

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -22,5 +22,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.3.1.0")]
-[assembly: AssemblyFileVersion("2.3.1.0")]
+[assembly: AssemblyVersion("2.3.2.0")]
+[assembly: AssemblyFileVersion("2.3.2.0")]
