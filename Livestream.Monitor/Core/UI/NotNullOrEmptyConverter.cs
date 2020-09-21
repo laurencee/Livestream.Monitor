569bc4c53596f553fad94a403669cfde54de312c
Laurence Evans <laurence.evanz@gmail.com>
2020-09-21 23:28:39 +1000

    Tile UI enchancements
    Show default background on tiles that have no image data
    Display a small border when mousing over tiles so the user can easily see what they're highlighting

---
+++ Livestream.Monitor/Core/UI/NotNullOrEmptyConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Livestream.Monitor.Core.UI
+{
+    [ValueConversion(typeof(string), typeof(bool))]
+    public class NotNullOrEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrEmpty(value as string);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
