19ac7394f4c4d36cd06895cf681b8a20e6f3af6d
Laurence0x03 <laurence.evanz@gmail.com>
2025-04-25 15:21:36 +1000

    Disable double click maximize on window

---
+++ Livestream.Monitor/Core/UI/DisableMetroDoubleClickBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace Livestream.Monitor.Core.UI;
+
+public static class DisableMetroDoubleClickBehavior
+{
+    public static readonly DependencyProperty EnabledProperty =
+        DependencyProperty.RegisterAttached(
+            "Enabled",
+            typeof(bool),
+            typeof(DisableMetroDoubleClickBehavior),
+            new PropertyMetadata(false, OnEnabledChanged));
+
+    public static void SetEnabled(DependencyObject element, bool value)
+        => element.SetValue(EnabledProperty, value);
+
+    public static bool GetEnabled(DependencyObject element)
+        => (bool)element.GetValue(EnabledProperty);
+
+    private static void OnEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (!(d is MetroWindow window) || !(bool)e.NewValue)
+            return;
+
+        window.Loaded += (s, args) =>
+        {
+            if (window.Template.FindName("PART_TitleBar", window) is UIElement titleBar)
+            {
+                titleBar.PreviewMouseLeftButtonDown += (s2, e2) =>
+                {
+                    if (e2.ClickCount == 2)
+                        e2.Handled = true;
+                };
+            }
+        };
+    }
+}
