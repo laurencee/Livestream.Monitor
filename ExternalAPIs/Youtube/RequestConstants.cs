57607b202b9c62edcfaf16221a9d2350a7237f42
Laurence0x03 <laurence.evanz@gmail.com>
2025-06-26 16:30:13 +1000

    fix: #88 Default youtube api result size to max (50)

--- ExternalAPIs/Youtube/RequestConstants.cs
+++ ExternalAPIs/Youtube/RequestConstants.cs
@@ -6,10 +6,10 @@
         public const string GoogleApiRoot = "https://www.googleapis.com/youtube/v3/";
 
         // fileDetails are only available to the video owner so we can't use that
-        public const string VideoLivestreamDetails = GoogleApiRoot + "videos?part=liveStreamingDetails,snippet,contentDetails,statistics";
-        public const string SearchChannelLiveVideos = GoogleApiRoot + "search?type=video&eventType=live&part=snippet,id&channelId={0}";
+        public const string VideoLivestreamDetails = GoogleApiRoot + "videos?part=liveStreamingDetails,snippet,contentDetails,statistics"; // maxResults not used for id lookup per api docs
+        public const string SearchChannelLiveVideos = GoogleApiRoot + "search?type=video&eventType=live&part=snippet,id&maxResults=50&channelId={0}";
         public const string GetChannelByHandle = GoogleApiRoot + "channels?forHandle={0}&part=id,snippet,contentDetails";
-        public const string GetChannels = GoogleApiRoot + "channels?part=id,snippet,contentDetails";
-        public const string GetPlaylistItems = GoogleApiRoot + "playlistItems?playlistId={0}&part=id,snippet,contentDetails,status";
+        public const string GetChannels = GoogleApiRoot + "channels?part=id,snippet,contentDetails&maxResults=50";
+        public const string GetPlaylistItems = GoogleApiRoot + "playlistItems?playlistId={0}&part=id,snippet,contentDetails,status"; // maxResults managed by PlaylistItemsQuery usage
     }
 }
